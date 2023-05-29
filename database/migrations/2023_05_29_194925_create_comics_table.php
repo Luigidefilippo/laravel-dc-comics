2f5506d9876703495984773b9a921b6032ee2280
Luigidefilippo <defilippoluigi65@gmail.com>
2023-05-29 21:58:47 +0200

    create table in db

--- database/migrations/2023_05_29_194925_create_comics_table.php
+++ database/migrations/2023_05_29_194925_create_comics_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title',30);
+            $table->text('description');
+            $table->string('price');
+            $table->string('type');
+            $table->text('thumb');
+            $table->string('sale_date');
+            $table->string('series');
             $table->timestamps();
         });
     }
