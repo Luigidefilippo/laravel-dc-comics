165f392df2dd178ac14be043e07a064d95414bdd
Luigidefilippo <defilippoluigi65@gmail.com>
2023-05-29 22:12:20 +0200

    popolutian seeder

--- database/seeders/ComicTableSeeder.php
+++ database/seeders/ComicTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,17 @@ class ComicTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics_array = config('comics');
+
+        foreach ($comics_array as $comic_item) {
+            $comic = new Comic();
+            $comic->title = $comic_item['title'];
+            $comic->description =$comic_item['description'];
+            $comic->price = $comic_item['price'];
+            $comic->type = $comic_item['type'];
+            $comic->thumb =$comic_item['thumb'];
+            $comic->sale_date =$comic_item['sale_date'];
+            $comic->series = $comic_item['series'];
+        }
     }
 }
