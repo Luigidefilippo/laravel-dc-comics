b16e71ece8ae80c52d9a1841cc807982234b7ca5
Luigidefilippo <defilippoluigi65@gmail.com>
2023-05-29 23:42:18 +0200

    Compleate singolar comics

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,8 +2,24 @@
 
 
 @section('content');
-<h2>Realtivo Comic:<br>
-    {{$comics->title}}
-</h2>
+<div class="container">
+    <h2>Realtivo Comic:<br>
+        {{$comics->title}}
+    </h2>
     
+    <img src="{{$comics->thumb}}" alt="">
+    <p>
+        {{$comics->description}}
+    </p>
+    <ul class="list-group m-4">
+        <li class="list-group-item active" aria-current="true">{{$comics->type}}</li>
+        <li class="list-group-item">{{$comics->sale_date}}</li>
+        <li class="list-group-item">{{$comics->series}}</li>
+        <li class="list-group-item">{{$comics->price}}</li>
+      </ul>
+</div>
+
+
+    
+
 @endsection
